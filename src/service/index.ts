64ec7349464ec0086d38b2cbb7622be66bf308fe
cx1801126003 <2716506732@qq.com>
2021-10-14 11:32:26 +0800

    feat(axios2): second axios package -- add interceptors

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,7 +3,25 @@ import CXRequest from './request'
 import { BASE_URL, TIME_OUT } from './request/config'
 const cxRequest = new CXRequest({
   baseURL: BASE_URL,
-  timeout: TIME_OUT
+  timeout: TIME_OUT,
+  interceptors: {
+    requestInterceptor: (config) => {
+      console.log('111')
+      return config
+    },
+    requestInterceptorCatch: (err) => {
+      console.log('222')
+      return err
+    },
+    responseInterceptor: (res) => {
+      console.log('333')
+      return res
+    },
+    responseInterceptorCatch: (err) => {
+      console.log('444')
+      return err
+    }
+  }
 })
 // console.log(BASE_URL)
 // console.log(TIME_OUT)
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,5 +1,16 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+interface CXRequestinterceptors {
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestInterceptorCatch?: (error: any) => any
+  responseInterceptor?: (res: AxiosResponse) => AxiosResponse
+  responseInterceptorCatch?: (error: any) => any
+}
+
+interface CXRequestConfig extends AxiosRequestConfig {
+  interceptors?: CXRequestinterceptors
+}
 
 class CXRequest {
   // request(): void {
@@ -9,8 +20,19 @@ class CXRequest {
   //   console.log('888')
   // }
   instance: AxiosInstance
-  constructor(config: AxiosRequestConfig) {
+  interceptors?: CXRequestinterceptors
+  constructor(config: CXRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
   }
 
   request(config: AxiosRequestConfig): void {

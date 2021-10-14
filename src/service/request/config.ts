91aa693f1acd83c989205b9278a8ba08bfbabf56
cx1801126003 <2716506732@qq.com>
2021-10-14 09:45:04 +0800

    style(process-env): change process-env

---
+++ src/service/request/config.ts
@@ -0,0 +1,26 @@
+// const BASE_URL = 'http://coderwhy.org/dev'
+// const BASE_NAME = 'coderwhy'
+
+// const BASE_URL = 'http://coderwhy.org/prod'
+// const BASE_NAME = 'kobe'
+
+// const BASE_URL = 'http://coderwhy.org/test'
+// const BASE_NAME = 'james'
+
+//2.根据process.env.NODE_ENV的值进行判断
+
+// let BASE_URL = ''
+// let BASE_NAME = ''
+
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = 'http://coderwhy.org/dev'
+//   BASE_NAME = 'coderwhy'
+// } else if (process.env.NODE_ENV === 'production') {
+//   BASE_URL = 'http://coderwhy.org/prod'
+//   BASE_NAME = 'kobe'
+// } else {
+//   BASE_URL = 'http://coderwhy.org/test'
+//   BASE_NAME = 'james'
+// }
+
+// export { BASE_URL, BASE_NAME }

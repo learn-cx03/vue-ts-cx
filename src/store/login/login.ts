cdbd498a63b5f16581a0c6d8be75234a125e857c
cx1801126003 <2716506732@qq.com>
2021-11-01 10:49:25 +0800

    feat(add all routes): add all routes

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -6,6 +6,7 @@ import {
   requestUserMenusByRoleId
 } from '@/service/login/login'
 import localCache from '@/utils/cache'
+import { mapMenusToRoutes } from '@/utils/map-menus'
 import router from '@/router'
 
 import { IAccount } from '@/service/login/type'
@@ -32,6 +33,8 @@ const loginModule: Module<ILoginState, IRootState> = {
     },
     changeUserMenus(state, userMenus: any) {
       state.userMenus = userMenus
+
+      mapMenusToRoutes(userMenus)
     }
   },
   actions: {
---
+++ src/utils/map-menus.ts
@@ -0,0 +1,17 @@
+import { RouteRecordRaw } from 'vue-router'
+
+export function mapMenusToRoutes(userMenus: any[]): RouteRecordRaw[] {
+  const routes: RouteRecordRaw[] = []
+
+  //1.先去加载所有的routes
+  const allRoutes: RouteRecordRaw[] = []
+  const routeFiles = require.context('../router/main', true, /\.ts/)
+  routeFiles.keys().forEach((key) => {
+    const route = require('../router/main' + key.split('.')[1])
+    allRoutes.push(route.default)
+  })
+  console.log(allRoutes)
+
+  //2.根据菜单获取需要添加的routes
+  return routes
+}

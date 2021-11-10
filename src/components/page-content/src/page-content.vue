287c78eb327e276a53be6cd841f103ea1342379d
cx1801126003 <2716506732@qq.com>
2021-11-10 11:33:35 +0800

    feat(page-content separate): page-content separate

---
+++ src/components/page-content/src/page-content.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="page-content">
+    <cx-table :listData="userList" v-bind="contentTableConfig">
+      <!--header中的插槽-->
+      <template #headerHandler>
+        <el-button type="primary" size="medium">新建用户</el-button>
+      </template>
+      <!--列表中的插槽-->
+      <template #status="scope"
+        ><el-button
+          plain
+          size="mini"
+          :type="scope.row.enable ? 'success' : 'danger'"
+          >{{ scope.row.enable ? '可用' : '不可用' }}</el-button
+        ></template
+      >
+      <template #createAt="scope">
+        <strong>{{ $filters.formatTime(scope.row.createAt) }}</strong>
+      </template>
+      <template #updateAt="scope">
+        <strong> {{ $filters.formatTime(scope.row.createAt) }}</strong>
+      </template>
+      <template #handler>
+        <div class="handler-btns">
+          <el-button size="mini" type="text"
+            ><el-icon><edit /></el-icon>编辑</el-button
+          >
+          <el-button size="mini" type="text "
+            ><el-icon><delete-filled /></el-icon>删除</el-button
+          >
+        </div>
+      </template>
+    </cx-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import CxTable from '@/base-ui/table'
+import { DeleteFilled, Edit } from '@element-plus/icons'
+import { useStore } from '@/store'
+
+export default defineComponent({
+  props: {
+    contentTableConfig: {
+      type: Object,
+      require: true
+    }
+  },
+  components: {
+    CxTable,
+    Edit,
+    DeleteFilled
+  },
+  setup() {
+    const store = useStore()
+    store.dispatch('system/getPageListAction', {
+      pageUrl: '/users/list',
+      queryInfo: {
+        offset: 0,
+        size: 10
+      }
+    })
+
+    const userList = computed(() => store.state.system.userList)
+    return {
+      userList
+    }
+  }
+})
+</script>
+<style lang="less" scoped>
+.page-content {
+  padding: 20px;
+  border-top: 20px solid #f5f5f5;
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,120 +1,33 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig"></page-search>
-    <div class="content">
-      <cx-table
-        :listData="userList"
-        :propList="propList"
-        :showIndexColumn="showIndexColumn"
-        :showSelectColumn="showSelectColumn"
-        :title="title"
-      >
-        <!--header中的插槽-->
-        <template #headerHandler>
-          <el-button type="primary" size="medium">新建用户</el-button>
-        </template>
-        <!--列表中的插槽-->
-        <template #status="scope"
-          ><el-button
-            plain
-            size="mini"
-            :type="scope.row.enable ? 'success' : 'danger'"
-            >{{ scope.row.enable ? '可用' : '不可用' }}</el-button
-          ></template
-        >
-        <template #createAt="scope">
-          <strong>{{ $filters.formatTime(scope.row.createAt) }}</strong>
-        </template>
-        <template #updateAt="scope">
-          <strong> {{ $filters.formatTime(scope.row.createAt) }}</strong>
-        </template>
-        <template #handler>
-          <div class="handler-btns">
-            <el-button size="mini" type="text"
-              ><el-icon><edit /></el-icon>编辑</el-button
-            >
-            <el-button size="mini" type="text "
-              ><el-icon><delete-filled /></el-icon>删除</el-button
-            >
-          </div>
-        </template>
-      </cx-table>
-    </div>
+    <page-content :contentTableConfig="contentTableConfig"></page-content>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
-import { searchFormConfig } from './config/search.config'
+import { defineComponent } from 'vue'
+
 import PageSearch from '@/components/page-search'
-import { useStore } from '@/store'
-import CxTable from '@/base-ui/table'
-import { DeleteFilled, Edit } from '@element-plus/icons'
+import PageContent from '@/components/page-content'
+
+import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
 
 export default defineComponent({
   name: 'user',
   setup() {
-    const store = useStore()
-    store.dispatch('system/getPageListAction', {
-      pageUrl: '/users/list',
-      queryInfo: {
-        offset: 0,
-        size: 10
-      }
-    })
-
-    const userList = computed(() => store.state.system.userList)
-
-    const title = '用户列表'
-
-    const propList = [
-      { prop: 'name', label: '用户名', minWidth: '100' },
-      { prop: 'realname', label: '真实姓名', minWidth: '100' },
-      { prop: 'cellphone', label: '手机号码', minWidth: '100' },
-      { prop: 'enable', label: '状态', minWidth: '100', slotName: 'status' },
-      {
-        prop: 'createAt',
-        label: '创建时间',
-        minWidth: '250',
-        slotName: 'createAt'
-      },
-      {
-        prop: 'updateAt',
-        label: '更新时间',
-        minWidth: '250',
-        slotName: 'updateAt'
-      },
-      {
-        label: '操作',
-        minWidth: '120',
-        slotName: 'handler'
-      }
-    ]
-
-    const showIndexColumn = true
-    const showSelectColumn = true
-
     return {
       searchFormConfig,
-      userList,
-      propList,
-      showIndexColumn,
-      showSelectColumn,
-      title
+
+      contentTableConfig
     }
   },
   components: {
     PageSearch,
-    CxTable,
-    Edit,
-    DeleteFilled
+    PageContent
   }
 })
 </script>
 
-<style scoped>
-.content {
-  padding: 20px;
-  border-top: 20px solid #f5f5f5;
-}
-</style>
+<style scoped></style>
